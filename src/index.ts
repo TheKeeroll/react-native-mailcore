3cedb27ace8d8771571fd839ddc957c97b826ac4
TheKeeroll <57570053+TheKeeroll@users.noreply.github.com>
2022-07-31 19:43:07 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,8 @@ class MailInstance{
       return RNMailCore.loginSmtp({
         hostname: this.mSMTPConfig.hostname,
         port: this.mSMTPConfig.port,
-        username: this.mUserCredentials.username,
-        password: this.mUserCredentials.password,
+        username: this.mUserCredentials!.username,
+        password: this.mUserCredentials!.password,
       }).then(()=>{
         this.mLoggedIn = true
         return Promise.resolve()
@@ -40,7 +40,7 @@ class MailInstance{
   }
 
   public FetchFolders(): Promise<Folder[]> {
-    return RNMailModule.GetFolders().then((folders: Folder[])=>{
+    return RNMailCore.getFolders().then((folders: Folder[])=>{
       this.mFolders = folders
       for(let folder of this.mFolders)
         folder.name = decode_imap_utf7(folder.path)
@@ -54,22 +54,26 @@ class MailInstance{
   }
 
 
-  public CreateFolder(folderName: string): Promise<void> {
-    return RNMailModule.CreateFolder(folderName).then(()=>{
-      return this.FetchFolders().then(()=>{
-        return Promise.resolve()
-      })
-    })
+  //public CreateFolder(folderName: string): Promise<void> {
+  //  return RNMailModule.CreateFolder(folderName).then(()=>{
+  //    return this.FetchFolders().then(()=>{
+  //      return Promise.resolve()
+  //    })
+  //  })
+  //}
+
+  //public RenameFolder(folderName: string, folderNewName: string): Promise<void> {
+  //  return RNMailModule.RenameFolder({old: folderName, 'new': folderNewName})
+  //}
+  public GetMails(folder: Folder, requestKind: number, lastLocalUID?: number): Promise<MailHeader[]> {
+    return RNMailCore.getMails({folder: folder.path, requestKind: requestKind, lastUID: lastLocalUID})
+  }
+  public GetMail(folderPath: string, requestKind: number) : Promise<Mail> {
+    return RNMailCore.getMail({folder: folderPath, requestKind})
   }
 
-  public RenameFolder(folderName: string, folderNewName: string): Promise<void> {
-    return RNMailModule.RenameFolder({old: folderName, 'new': folderNewName})
-  }
-  public GetMails(folder: Folder, requestKind: number, lastLocalUID?: number): Promise<MailHeader[]> {
-    return RNMailModule.GetMails({path: folder.path, requestKind: requestKind, lastUID: lastLocalUID})
-  }
-  public GetMail(folderPath: string, requestKind: number, messageUID: number) : Promise<Mail> {
-    return RNMailModule.GetMail({folder: folderPath, requestKind, messageUID})
+  public GetAttachment(fileName: string, folder: Folder, messageUID: number): Promise<any> {
+    return RNMailCore.getAttachment({filename: fileName, folder: folder.path, messageId: messageUID})
   }
 
 }
